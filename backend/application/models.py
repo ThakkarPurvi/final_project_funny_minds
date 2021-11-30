eadbb2d2b10f6caeabd9b7acb33c5b3278226c4f
Ubuntu <purvi@DevJenkins.l2qd0vbrbuhuhgbbbu1dozytpd.ax.internal.cloudapp.net>
2021-11-30 18:07:50 +0000

    adding updates

--- backend/application/models.py
+++ backend/application/models.py
@@ -5,9 +5,10 @@ class Young_Mind(db.Model):
     name = db.Column(db.String(50), nullable=False)
     country = db.Column(Text(50), nullable=False)
     dob = db.Column(db.Date, nullable=False)
-	jokes = db.relationship(Young_Mind, backref'young_mind')
+	jokes = db.relationship("Jokes", backref='young_mind')
  
 class Jokes(db.Model):
     id = Column(db.Integer, primary_key=True)
-    joke_Category= db.relationship('Category')
-    young_mind_id = db.Column(db.Integer, ForeignKey('young_Mind.id'), nullable=False)
+    joke_Category = db.Column(db.String(50), nullable=False)
+    joke_description = db.Column(db.String(500), nullable=False)
+    young_mind_id = db.Column(db.Integer, db.ForeignKey('young_mind.id'), nullable=False)

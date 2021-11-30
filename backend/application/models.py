396854a9c5385d3ff9887280383e3cdadb910463
Ubuntu <purvi@DevJenkins.l2qd0vbrbuhuhgbbbu1dozytpd.ax.internal.cloudapp.net>
2021-11-30 18:40:30 +0000

    adding updates

--- backend/application/models.py
+++ backend/application/models.py
@@ -3,7 +3,7 @@ from application import db
 class Young_Mind(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    country = db.Column(Text(50), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
     dob = db.Column(db.Date, nullable=False)
     jokes = db.relationship("Jokes", backref="Young_Mind")
 
